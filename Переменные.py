932f9501d5680cee77cd3b93a5d068feaa02b392
Fedor-snow <117363957+Fedor-snow@users.noreply.github.com>
2024-10-04 15:03:24 +0300

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,9 @@
+cours = "python"
+tasks = 12
+hours = 1.5
+timeTask = 12/1.5
+print("Курс:" ,cours,", всего задач:"
+              ,tasks,", затрачено часов:"
+              ,hours,", среднее время выполнения"
+              ,timeTask,"часа.")
+
